29c09d9e23a87aaff34a1e4bb591c3852c3b8cbb
Wayne Curts <emmanuelogheneroro98@gmail.com>
2026-01-14 09:06:34 +0100

    Initial system-log-analyzer project structure

--- app/main.py
+++ app/main.py
@@ -1,6 +1,30 @@
+import sys
+
 def analyze_log(file_path):
-    """
-    Reads a log file and returns number of lines.
-    """
+    counts = {
+        "INFO": 0,
+        "WARNING": 0,
+        "ERROR": 0
+    }
+
     with open(file_path, "r") as file:
-        return len(file.readlines())
+        for line in file:
+            if line.startswith("INFO"):
+                counts["INFO"] += 1
+            elif line.startswith("WARNING"):
+                counts["WARNING"] += 1
+            elif line.startswith("ERROR"):
+                counts["ERROR"] += 1
+
+    return counts
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <logfile>")
+        sys.exit(1)
+
+    log_file = sys.argv[1]
+    results = analyze_log(log_file)
+
+    print("Log Summary:")
+    for level, count in results.items():
+        print(f"{level}: {count}")
